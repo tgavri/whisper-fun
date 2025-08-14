d887559a8a34f378983f9ec5a6bb81afcafa4637
tgavri <thom54s8@stud.kea.dk>
2025-08-14 12:13:31 +0200

    shell.nix script added with CUDA SUPPORT git add .! :D

--- shell.nix
+++ shell.nix
@@ -1,14 +1,24 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ 
-      python313
-      python313Packages.openai-whisper
-      python313Packages.streamlit
-      python313Packages.ffmpeg-python
-      python313Packages.flask
-      # nix repl, :l <nixpkgs>, søg
-      python313Packages.ffmpeg-python
-      python313Packages.srt
-    ];
+{ pkgs ? import <nixpkgs> {
+    config = { allowUnfree = true; cudaSupport = false; };
+  }
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python313
+    python313Packages.pytorch-bin
+    python313Packages.openai-whisper
+    python313Packages.ffmpeg-python
+    python313Packages.flask
+    python313Packages.srt
+    cudatoolkit
+  ];
+
+  shellHook = ''
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/nvidia/current:$LD_LIBRARY_PATH"
+    export LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/nvidia/current:$LIBRARY_PATH"
+    echo "Set CUDA_PATH, LD_LIBRARY_PATH, LIBRARY_PATH to NVIDIA driver libs only"
+  '';
 }
 
